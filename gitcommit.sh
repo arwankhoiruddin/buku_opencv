c834cd79ba7c59e7bfc99a8fa1d11a328ed11cca
arwan khoiruddin <arwan.khoiruddin@gmail.com>
2016-01-12 11:19:34 +0700

    images are included in gambar folder

---
+++ gitcommit.sh
@@ -0,0 +1,17 @@
+rm opencv_arwan_book.aux
+rm opencv_arwan_book.bbl
+rm opencv_arwan_book.bcf
+rm opencv_arwan_book.blg
+rm opencv_arwan_book.lof
+rm opencv_arwan_book.log
+rm opencv_arwan_book.lot
+rm opencv_arwan_book.out
+rm opencv_arwan_book.pdf
+rm opencv_arwan_book.run.xml
+rm opencv_arwan_book.synctex.gz
+rm opencv_arwan_book.toc
+
+git init
+git add . -A
+git commit -m 'images are included in gambar folder'
+git push origin master
